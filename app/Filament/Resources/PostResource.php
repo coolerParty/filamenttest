285d2181dcefa8fec68ffa0f00b52ec76c83296a
coolerParty <alexmeiers2014@gmail.com>
2022-04-12 09:25:15 +0800

    Files Added

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Forms\Components\Card;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema(
+                Card::make()->columns(1)->schema([
+                    Forms\Components\TextInput::make('title')
+                        ->autocomplete($autocomplete = 'on')
+                        ->autofocus()
+                        ->placeholder('title') 
+                        ->type($type = 'text')
+                        ->required(),
+                    Forms\Components\RichEditor::make('body')
+                        ->disableToolbarButtons($buttons = []) 
+                        ->enableToolbarButtons($buttons = [])
+                        ->required()
+                ])
+            );
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('body')->html()
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
